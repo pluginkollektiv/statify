4aa8f99d74346b9ff03d62fbb25147b6c7a30f38
bueltge <frank@bueltge.de>
2016-12-19 14:03:56 +0100

    First steps to wp Codex, #16

--- statify.php
+++ statify.php
@@ -1,43 +1,27 @@
 <?php
-/*
-Plugin Name: Statify
-Description: Compact, easy-to-use and privacy-compliant stats plugin for WordPress.
-Text Domain: statify
-Domain Path: /lang
-Author:      pluginkollektiv
-Author URI:  http://pluginkollektiv.org
-Plugin URI:  https://wordpress.org/plugins/statify
-License:     GPLv3 or later
-Version:     1.4.3
-*/
-
-/*
-Copyright (C)  2011-2015 Sergej Müller
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License along
-with this program; if not, write to the Free Software Foundation, Inc.,
-51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+/**
+ * Plugin Name: Statify
+ * Description: Compact, easy-to-use and privacy-compliant stats plugin for WordPress.
+ * Text Domain: statify
+ * Domain Path: /lang
+ * Author:      pluginkollektiv
+ * Author URI:  http://pluginkollektiv.org
+ * Plugin URI:  https://wordpress.org/plugins/statify
+ * License:     GPLv3 or later
+ * Version:     1.4.3
+ *
+ * Copyright (C)  2011-2016 Sergej Müller, pluginkollektiv
+ */
 
 
 /* Quit */
-defined('ABSPATH') OR exit;
+defined( 'ABSPATH' ) || exit;
 
 
 /*  Constants */
-define('STATIFY_FILE', __FILE__);
-define('STATIFY_DIR', dirname(__FILE__));
-define('STATIFY_BASE', plugin_basename(__FILE__));
+define( 'STATIFY_FILE', __FILE__ );
+define( 'STATIFY_DIR', dirname( __FILE__ ) );
+define( 'STATIFY_BASE', plugin_basename( __FILE__ ) );
 
 
 /* Hooks */
@@ -45,56 +29,62 @@ add_action(
 	'plugins_loaded',
 	array(
 		'Statify',
-		'instance'
+		'instance',
 	)
 );
 register_activation_hook(
 	STATIFY_FILE,
 	array(
 		'Statify_Install',
-		'init'
+		'init',
 	)
 );
 register_deactivation_hook(
-    STATIFY_FILE,
-    array(
-        'Statify_Deactivate',
-        'init'
-    )
+	STATIFY_FILE,
+	array(
+		'Statify_Deactivate',
+		'init',
+	)
 );
 register_uninstall_hook(
 	STATIFY_FILE,
 	array(
 		'Statify_Uninstall',
-		'init'
+		'init',
 	)
 );
 
 
 /* Autoload */
-spl_autoload_register('statify_autoload');
+spl_autoload_register( 'statify_autoload' );
 
-function statify_autoload($class) {
-    $plugin_classes = array(
-        'Statify',
-        'Statify_Backend',
-        'Statify_Frontend',
-        'Statify_Dashboard',
-        'Statify_Install',
-        'Statify_Uninstall',
-        'Statify_Deactivate',
-        'Statify_Table',
-        'Statify_XMLRPC',
-        'Statify_Cron'
-    );
+/**
+ * Include classes via autoload.
+ *
+ * @param string $class Name of an class-file name, without file extension.
+ */
+function statify_autoload( $class ) {
 
-    if ( in_array($class, $plugin_classes) ) {
-        require_once(
-            sprintf(
-                '%s/inc/%s.class.php',
-                STATIFY_DIR,
-                strtolower($class)
-            )
-        );
-    }
+	$plugin_classes = array(
+		'Statify',
+		'Statify_Backend',
+		'Statify_Frontend',
+		'Statify_Dashboard',
+		'Statify_Install',
+		'Statify_Uninstall',
+		'Statify_Deactivate',
+		'Statify_Table',
+		'Statify_XMLRPC',
+		'Statify_Cron',
+	);
+
+	if ( in_array( $class, $plugin_classes, true ) ) {
+		require_once(
+			sprintf(
+				'%s/inc/%s.class.php',
+				STATIFY_DIR,
+				strtolower( $class )
+			)
+		);
+	}
 }
