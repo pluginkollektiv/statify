b6cb9f5b3dd6c0e38811c303a309e0cd1cb4d22b
bueltge <frank@bueltge.de>
2016-12-19 14:26:22 +0100

    next steps to wp Codex, #16

--- inc/statify_backend.class.php
+++ inc/statify_backend.class.php
@@ -1,35 +1,29 @@
 <?php
-
-
-/* Quit */
-defined('ABSPATH') OR exit;
-
+/** Quit */
+defined( 'ABSPATH' ) || exit;
 
 /**
-* Statify_Backend
-*
-* @since 1.4.0
-*/
-
-class Statify_Backend
-{
-
+ * Statify_Backend
+ *
+ * @since 1.4.0
+ */
+class Statify_Backend {
 
 	/**
-	* Add plugin meta links
-	*
-	* @since   0.1.0
-	* @change  1.4.0
-	*
-	* @param   array   $input  Registered links
-	* @param   string  $file   Current plugin file
-	* @return  array           Merged links
-	*/
+	 * Add plugin meta links
+	 *
+	 * @since    0.1.0
+	 * @version  1.4.0
+	 *
+	 * @param   array  $input Registered links.
+	 * @param   string $file  Current plugin file.
+	 *
+	 * @return  array           Merged links
+	 */
+	public static function add_meta_link( $input, $file ) {
 
-	public static function add_meta_link($input, $file)
-	{
 		/* Restliche Plugins? */
-		if ( $file !== STATIFY_BASE ) {
+		if ( STATIFY_BASE !== $file ) {
 			return $input;
 		}
 
@@ -41,37 +35,34 @@ class Statify_Backend
 		);
 	}
 
-
 	/**
-	* Add plugin action links
-	*
-	* @since   0.1.0
-	* @change  1.4.0
-	*
-	* @param   array   $input  Registered links
-	* @return  array           Merged links
-	*/
+	 * Add plugin action links
+	 *
+	 * @since   0.1.0
+	 * @change  1.4.0
+	 *
+	 * @param   array $input Registered links.
+	 *
+	 * @return  array           Merged links
+	 */
+	public static function add_action_link( $input ) {
 
-	public static function add_action_link($input)
-	{
 		/* Rechte? */
-		if ( ! current_user_can('edit_dashboard') ) {
+		if ( ! current_user_can( 'edit_dashboard' ) ) {
 			return $input;
 		}
 
-		/* Zusammenführen */
+		/** Zusammenführen */
 		return array_merge(
 			$input,
 			array(
 				sprintf(
+					/** @lang  Disable language injection for Url query argument. */
 					'<a href="%s">%s</a>',
-					add_query_arg(
-						array(
-							'edit' => 'statify_dashboard#statify_dashboard'
-						),
-						admin_url('/')
+					add_query_arg( array( 'edit' => 'statify_dashboard#statify_dashboard' ),
+						admin_url( '/' )
 					),
-					__('Settings')
+					esc_html__( 'Settings', 'statify' )
 				)
 			)
 		);
