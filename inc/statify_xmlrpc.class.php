f582319c3bb559bc63df3a0f0afc0e3c8d455fce
bueltge <frank@bueltge.de>
2016-12-20 13:22:32 +0100

    Switch to wp_json_encode, next steps to wp Codex, #16

--- inc/statify_xmlrpc.class.php
+++ inc/statify_xmlrpc.class.php
@@ -1,34 +1,29 @@
 <?php
-
-
-/* Quit */
-defined('ABSPATH') OR exit;
-
+/** Quit */
+defined( 'ABSPATH' ) || exit;
 
 /**
-* Statify_XMLRPC
-*
-* @since 1.1
-*/
-
-class Statify_XMLRPC
-{
-
+ * Statify_XMLRPC
+ *
+ * @since 1.1
+ */
+class Statify_XMLRPC {
 
 	/**
-	* Erweiterung der XMLRPC-Methode
-	*
-	* @since   1.1.0
-	* @change  1.1.0
-	*
-	* @return  array  $methods  Array ohne Plugin-Callback
-	* @return  array  $methods  Array mit Plugin-Callback
-	*/
+	 * Enhancement from the XMLRPC-method.
+	 *
+	 * @since   1.1.0
+	 * @version 1.1.0
+	 *
+	 * @param   array $methods Array without Plugin-Callback.
+	 *
+	 * @return  array $methods Array with Plugin-Callback.
+	 */
+	public static function xmlrpc_methods( $methods ) {
 
-	public static function xmlrpc_methods($methods) {
 		$methods['statify.getStats'] = array(
 			__CLASS__,
-			'xmlrpc_callback'
+			'xmlrpc_callback',
 		);
 
 		return $methods;
@@ -36,39 +31,40 @@ class Statify_XMLRPC
 
 
 	/**
-	* Ausführung der XMLRPC-Anfrage
-	*
-	* @since   1.1.0
-	* @change  1.2.5
-	*
-	* @param   array   $args  Array mit Parametern (Zugangsdaten)
-	* @return  string         String mit Ergebnissen
-	*/
+	 * Ausführung der XMLRPC-Anfrage
+	 *
+	 * @since   1.1.0
+	 * @version 1.2.5
+	 *
+	 * @param   array $args Array with parameters (access data).
+	 *
+	 * @return  string         String with results.
+	 */
+	public static function xmlrpc_callback( $args ) {
 
-	public static function xmlrpc_callback($args) {
-		/* Keine Zugangsdaten? */
-		if ( empty($args[0]) OR empty($args[1]) ) {
+		/** No access data? */
+		if ( empty( $args[0] ) || empty( $args[1] ) ) {
 			return '{"error": "Empty login data"}';
 		}
 
-		/* Nutzer einloggen */
-		$user = wp_authenticate($args[0], $args[1]);
+		/** Login */
+		$user = wp_authenticate( $args[0], $args[1] );
 
-		/* Falsche Zugangsdaten */
-		if ( ! $user OR is_wp_error($user) ) {
+		/** Wrong access data */
+		if ( ! $user || is_wp_error( $user ) ) {
 			return '{"error": "Incorrect login"}';
 		}
 
-		/* Berechtigung prüfen */
-		if ( ! user_can($user, 'edit_dashboard') ) {
+		/** Check access rights. */
+		if ( ! user_can( $user, 'edit_dashboard' ) ) {
 			return '{"error": "User can check failed"}';
 		}
 
-		/* Leer? */
+		/** Empty? */
 		if ( ! $data = Statify_Dashboard::get_stats() ) {
 			return '{"error": "No data"}';
 		}
 
-		return json_encode($data['visits']);
+		return wp_json_encode( $data['visits'] );
 	}
 }
