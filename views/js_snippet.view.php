25feb69d65677ed68b07f2282047c81ce808b652
bueltge <frank@bueltge.de>
2016-12-20 13:26:51 +0100

    sanitize the home url and codex, #16

--- views/js_snippet.view.php
+++ views/js_snippet.view.php
@@ -1,26 +1,23 @@
 <?php
+/** Quit */
+class_exists( 'Statify' ) || exit; ?>
 
 
-/* Quit */
-class_exists('Statify') OR exit; ?>
+	<!-- Stats by http://statify.de -->
+	<script type="text/javascript">
+		(function () {
+			var e = document.createElement('script'),
+				s = document.getElementsByTagName('script')[0],
+				r = encodeURIComponent(document.referrer),
+				t = encodeURIComponent(location.pathname + location.search),
+				p = '?statify_referrer=' + r + '&statify_target=' + t;
 
+			e.async = true;
+			e.type = 'text/javascript';
+			e.src = "<?php echo esc_url( home_url( '/', 'relative' ) ); ?>" + p;
 
-<!-- Stats by http://statify.de -->
-<script type="text/javascript">
-    (function() {
-        var e = document.createElement('script'),
-            s = document.getElementsByTagName('script')[0],
-            r = encodeURIComponent(document.referrer),
-            t = encodeURIComponent(location.pathname + location.search),
-            p = '?statify_referrer=' + r + '&statify_target=' + t;
+			s.parentNode.insertBefore(e, s);
+		})();
+	</script>
 
-        e.async = true;
-        e.type = 'text/javascript';
-        e.src = "<?php echo home_url('/', 'relative'); ?>" + p;
-
-        s.parentNode.insertBefore(e, s);
-    })();
-</script>
-
-
-<?php /* Markup space */ ?>
+<?php /** Markup space */ ?>
