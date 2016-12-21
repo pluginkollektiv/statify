146d8a837ee5042db8fbfb512ee3171f78771219
bueltge <frank@bueltge.de>
2016-12-21 16:34:22 +0100

    Add fct check for older wp version

--- inc/statify_frontend.class.php
+++ inc/statify_frontend.class.php
@@ -122,7 +122,9 @@ class Statify_Frontend extends Statify {
 	 */
 	private static function _skip_tracking() {
 
-		apply_filters_deprecated( 'statify_skip_tracking', array( '' ), '1.5.0', 'statify__skip_tracking' );
+		if ( function_exists( 'apply_filters_deprecated' ) ) {
+			apply_filters_deprecated( 'statify_skip_tracking', array( '' ), '1.5.0', 'statify__skip_tracking' );
+		}
 		/* Skip tracking via Hook */
 		if ( ( $skip_hook = apply_filters( 'statify__skip_tracking', null ) ) !== null ) {
 			return $skip_hook;
