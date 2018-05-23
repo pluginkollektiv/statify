994c58923e4a6a28c1f8b75e328be8481c666bd5
Stefan Kalscheuer <stefan@stklcode.de>
2018-05-23 21:41:44 +0200

    Separate data from script tag and bulletproof JS (#92)
    * Separate data attribute from script tag
    
    This prevents issues with optimization techniques that remove the original
    <script> tag from the markup while the script content relies on its presence.
    
    * Add try-catch to JS snippet
    
    If the script fails for whatever reason (e.g. required data tag has been
    removed), it should not mess up any further scripts executed.

--- views/js-snippet.php
+++ views/js-snippet.php
@@ -10,11 +10,12 @@
 class_exists( 'Statify' ) || exit; ?>
 
 	<!-- Stats by http://statify.de -->
-	<script
-		id="statify-js-snippet"
-		data-home-url="<?php echo esc_url( home_url( '/', 'relative' ) ); ?>"
-		type="text/javascript"
-		src="<?php echo esc_attr( plugins_url( 'js/snippet.js', STATIFY_FILE ) ); ?>">
+	<div id="statify-js-snippet"
+		 style="display:none"
+		 data-home-url="<?php echo esc_url( home_url( '/', 'relative' ) ); ?>">
+	</div>
+	<script type="text/javascript"
+			src="<?php echo esc_attr( plugins_url( 'js/snippet.js', STATIFY_FILE ) ); ?>">
 	</script>
 
 <?php /* Markup space */ ?>
