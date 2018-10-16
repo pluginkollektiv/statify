74ca426facdc002f0284df23eacf12a0f0690cbe
Stefan Kalscheuer <stefan@stklcode.de>
2018-10-16 19:28:40 +0200

    Reset AJAX request from jQuery back to vanilla JS with XmlHttpRequest

--- js/snippet.js
+++ js/snippet.js
@@ -1,13 +1,12 @@
 try {
-	jQuery.ajax( {
-		type: 'POST',
-		url : statify_ajax.url,
-		data: {
-			_ajax_nonce     : statify_ajax.nonce,
-			action          : 'statify_track',
-			statify_referrer: encodeURIComponent( document.referrer ),
-			statify_target  : encodeURIComponent( location.pathname + location.search )
-		}
-	} );
+	var statifyReq = new XMLHttpRequest();
+	statifyReq.open( 'POST', statify_ajax.url, true );
+	statifyReq.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded;' );
+	statifyReq.send(
+		'_ajax_nonce=' + statify_ajax.nonce +
+		'&action=statify_track' +
+		'&statify_referrer=' + encodeURIComponent( document.referrer ) +
+		'&statify_target=' + encodeURIComponent( location.pathname + location.search )
+	);
 } catch ( e ) {
 }
