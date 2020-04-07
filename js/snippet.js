1d38eaa8db587796fa6cffd11501e4728f8d9c01
Stefan Kalscheuer <stefan@stklcode.de>
2020-04-07 12:34:40 +0200

    overhaul JS and CSS files to match WPCS (syntax only)

--- js/snippet.js
+++ js/snippet.js
@@ -1,11 +1,14 @@
-try {
-	const statifyReq = new XMLHttpRequest();
-	statifyReq.open(
-		'GET',
-		document.getElementById('statify-js-snippet').getAttribute('data-home-url')
-		+ '?statify_referrer=' + encodeURIComponent(document.referrer)
-		+ '&statify_target=' + encodeURIComponent(location.pathname + location.search)
-	);
-	statifyReq.send(null);
-} catch (e) {
-}
+( function() {
+	var statifyReq;
+	try {
+		statifyReq = new XMLHttpRequest();
+		statifyReq.open(
+			'GET',
+			document.getElementById( 'statify-js-snippet' ).getAttribute( 'data-home-url' ) +
+			'?statify_referrer=' + encodeURIComponent( document.referrer ) +
+			'&statify_target=' + encodeURIComponent( location.pathname + location.search )
+		);
+		statifyReq.send( null );
+	} catch ( e ) {
+	}
+}() );
