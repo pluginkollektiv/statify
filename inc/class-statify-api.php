pluginkollektiv/statify

commit 92a229425cadb6e0be5ec513f455abdb570d6c9b

migrate dashboard widget to REST API (#219) (#226)
Dynamically fetch data for chart and top lists from WP API.
We add some placeholders in the backend code to hopefully reduce
layout shifts and replace.

inc/class-statify-api.php
@@ -24,6 +24,7 @@ class Statify_Api extends Statify {
 	 */
 	const REST_NAMESPACE   = 'statify/v1';
 	const REST_ROUTE_TRACK = 'track';
+	const REST_ROUTE_STATS = 'stats';
 
 	/**
 	 * Initialize REST API routes.
@@ -39,6 +40,16 @@ class Statify_Api extends Statify {
 				'accept_json'         => true,
 				'callback'            => array( __CLASS__, 'track_visit' ),
 				'permission_callback' => '__return_true',
+			)
+		);
+
+		register_rest_route(
+			self::REST_NAMESPACE,
+			self::REST_ROUTE_STATS,
+			array(
+				'methods'             => WP_REST_Server::READABLE,
+				'callback'            => array( __CLASS__, 'get_stats' ),
+				'permission_callback' => array( __CLASS__, 'user_can_see_stats' ),
 			)
 		);
 
@@ -99,5 +110,43 @@ class Statify_Api extends Statify {
 		}
 
 		return new WP_REST_Response( null, 204 );
+	}
+
+	/**
+	 * Get statistics.
+	 *
+	 * @param WP_REST_Request $request The request.
+	 *
+	 * @return WP_REST_Response The response.
+	 */
+	public static function get_stats( $request ) {
+		$refresh = '1' === $request->get_param( 'refresh' );
+
+		$stats  = Statify_Dashboard::get_stats( $refresh );
+
+		$visits          = $stats['visits'];
+		$stats['visits'] = array();
+		foreach ( $visits as $v ) {
+			$stats['visits'][ Statify::parse_date( $v['date'] ) ] = intval( $v['count'] );
+		}
+
+		foreach ( $stats['referrer'] as &$r ) {
+			$r['count'] = intval( $r['count'] );
+		}
+
+		foreach ( $stats['target'] as &$t ) {
+			$t['count'] = intval( $t['count'] );
+		}
+
+		if ( isset( $stats['visit_totals'] ) ) {
+			$stats['totals'] = array(
+				'today'   => intval( $stats['visit_totals']['today'] ),
+				'alltime' => intval( $stats['visit_totals']['since_beginning']['count'] ),
+				'since'   => Statify::parse_date( $stats['visit_totals']['since_beginning']['date'] ),
+			);
+			unset( $stats['visit_totals'] );
+		}
+
+		return new WP_REST_Response( $stats );
 	}
 }

inc/class-statify.php
@@ -197,6 +197,20 @@ class Statify {
 	}
 
 	/**
+	 * Check whether the current user can see stats.
+	 *
+	 * @return boolean TRUE, if the user can see stats. FALSE otherwise.
+	 *
+	 * @since 1.9.0 extracted into method for reusability
+	 *
+	 * @hook  boolean  statify__user_can_see_stats
+	 * @see   https://wordpress.org/plugins/statify/
+	 */
+	public static function user_can_see_stats() {
+		return apply_filters( 'statify__user_can_see_stats', current_user_can( 'edit_dashboard' ) );
+	}
+
+	/**
 	 * Rules to skip the tracking.
 	 *
 	 * @hook   boolean  statify__skip_tracking

css/dashboard.css
@@ -5,14 +5,15 @@
 	flex: 0 0 100%;
 	direction: rtl;
 	height: 140px;
-	margin: 0 -4px;
+	margin: 1em -0.5em;
 	overflow-x: auto;
 	overflow-y: hidden;
 	text-align: center;
 }
 
-#statify_chart_data {
-	display: none;
+#statify_chart .spinner {
+	float: none;
+	margin: 1em;
 }
 
 #statify_chart .ct-label.ct-vertical.ct-start {
@@ -64,7 +65,6 @@
 	flex-wrap: wrap;
 	height: 1%;
 	margin: 0;
-	padding-bottom: 0;
 	overflow: hidden;
 	position: relative;
 }
@@ -98,6 +98,12 @@
 #statify_dashboard td {
 	padding: 8px 0 0;
 	line-height: 1.4;
+}
+
+#statify_dashboard tr.placeholder td {
+	background-color: #f5f5f5;
+	border-radius: 1em;
+	width: 10em;
 }
 
 #statify_dashboard td.b {

inc/class-statify-dashboard.php
@@ -30,20 +30,16 @@ class Statify_Dashboard extends Statify {
 	 * Dashboard widget initialize
 	 *
 	 * @since   0.1.0
-	 * @version 2016-12-21
-	 *
-	 * @wp-hook boolean  statify__user_can_see_stats
-	 * @see     https://wordpress.org/plugins/statify/
 	 */
 	public static function init() {
 
 		// Filter user_can_see_stats.
-		if ( ! apply_filters( 'statify__user_can_see_stats', current_user_can( 'edit_dashboard' ) ) ) {
+		if ( ! self::user_can_see_stats() ) {
 			return;
 		}
 
 		// Check if user can edit the widget.
-		$can_edit = apply_filters( 'statify__user_can_see_stats', current_user_can( 'edit_dashboard' ) );
+		$can_edit = self::user_can_see_stats();
 
 		// Load textdomain.
 		load_plugin_textdomain(
@@ -129,11 +125,8 @@ class Statify_Dashboard extends Statify {
 		);
 		wp_register_script(
 			'statify_chart_js',
-			plugins_url(
-				'js/dashboard.min.js',
-				STATIFY_FILE
-			),
-			array( 'jquery', 'chartist_tooltip_js' ),
+			plugins_url( 'js/dashboard.min.js', STATIFY_FILE ),
+			array( 'wp-api-fetch', 'chartist_tooltip_js' ),
 			self::$_plugin_version,
 			true
 		);
@@ -141,10 +134,16 @@ class Statify_Dashboard extends Statify {
 		// Localize strings.
 		wp_localize_script(
 			'statify_chart_js',
-			'statify_translations',
+			'statifyDashboard',
 			array(
-				'pageview'  => strip_tags( esc_html__( 'Pageview', 'statify' ) ),
-				'pageviews' => strip_tags( esc_html__( 'Pageviews', 'statify' ) ),
+				'i18n'  => array(
+					'error'        => esc_html__( 'Error loading data.', 'statify' ),
+					'nodata'       => esc_html__( 'No data available.', 'statify' ),
+					'pageview'     => esc_html__( 'Pageview', 'statify' ),
+					'pageviews'    => esc_html__( 'Pageviews', 'statify' ),
+					'since'        => esc_html__( 'since', 'statify' ),
+					'today'        => esc_html__( 'today', 'statify' ),
+				),
 			)
 		);
 	}

js/dashboard.js
@@ -1,93 +1,194 @@
 ( function() {
 	// Initialize.
-	var labels = [];
-	var data = [];
-	var statifyDataTable = jQuery( '#statify_chart_data' );
-	var maxValue;
-	var chartWidth;
-	var fullWidth = true;
-	var pointRadius = 4;
-	var chart;
+	var chartElem = document.getElementById( 'statify_chart' );
+	var referrerTable = document.querySelector( '#statify_dashboard .table.referrer table tbody' );
+	var targetTable = document.querySelector( '#statify_dashboard .table.target table tbody' );
+	var totalsTable = document.querySelector( '#statify_dashboard .table.total table tbody' );
+	var refreshBtn = document.getElementById( 'statify_refresh' );
 
-	// Abort if no data is present.
-	if ( ! statifyDataTable.length ) {
+	// Abort if config or target element is not present.
+	if ( typeof statifyDashboard === 'undefined' || typeof chartElem === 'undefined' ) {
 		return;
 	}
 
-	// Collect data from hidden table.
-	jQuery( 'th', statifyDataTable ).each( function() {
-		labels.push( jQuery( this ).text() );
-	} );
+	/**
+	 * Update the dashboard widget
+	 *
+	 * @param {boolean} refresh Force refresh.
+	 */
+	function updateDashboard( refresh ) {
+		// Disable refresh button.
+		if ( refreshBtn ) {
+			refreshBtn.disabled = true;
+		}
 
-	jQuery( 'td', statifyDataTable ).each( function() {
-		data.push( jQuery( this ).text() );
-	} );
+		// Load data from API.
+		wp.apiFetch( { path: '/statify/v1/stats' + ( refresh ? '?refresh=1' : '' ) } ).then( function( data ) {
+			var labels = Object.keys( data.visits );
+			var values = Object.values( data.visits );
+			// Determine maximum value for scaling.
+			var maxValue = Math.max.apply( Math, values );
+			var fullWidth = true;
+			var pointRadius = 4;
+			var chart;
+			var rows;
+			var row;
+			var i;
 
-	// Determine maximum value for scaling.
-	maxValue = Math.max.apply( Math, data );
+			// Remove the loading content.
+			chartElem.innerHTML = '';
 
-	// Adjust display according if there are too many values to display redable.
-	chartWidth = jQuery( '#statify_chart' ).width();
-	if ( chartWidth < data.length * 4 ) {
-		// Make chart scrollable, if 2px points are overlapping.
-		fullWidth = false;
-		pointRadius = 3;
-	} else if ( chartWidth < data.length * 8 ) {
-		// Shrink datapoints if 4px is overlapping, but 2 is not.
-		pointRadius = 2;
+			// Adjust display according if there are too many values to display readable.
+			if ( labels.length === 0 ) {
+				chartElem.innerHTML = '<p>' + statifyDashboard.i18n.nodata + '</p>';
+				return;
+			} else if ( chartElem.clientWidth < labels.length * 4 ) {
+				// Make chart scrollable, if 2px points are overlapping.
+				fullWidth = false;
+				pointRadius = 3;
+			} else if ( chartElem.clientWidth < labels.length * 8 ) {
+				// Shrink datapoints if 4px is overlapping, but 2 is not.
+				pointRadius = 2;
+			}
+
+			// Draw chart.
+			chart = new Chartist.LineChart( '#statify_chart', {
+				labels: labels,
+				series: [
+					values,
+				],
+			}, {
+				low: 0,
+				showArea: true,
+				fullWidth: fullWidth,
+				width: ( fullWidth ? undefined : 5 * data.length ),
+				axisX: {
+					showGrid: false,
+					showLabel: false,
+					offset: 0,
+				},
+				axisY: {
+					showGrid: true,
+					showLabel: true,
+					type: Chartist.FixedScaleAxis,
+					low: 0,
+					high: maxValue + 1,
+					ticks: [
+						0,
+						Math.round( maxValue * 1 / 4 ),
+						Math.round( maxValue * 2 / 4 ),
+						Math.round( maxValue * 3 / 4 ),
+						maxValue,
+					],
+					offset: 30,
+				},
+				plugins: [
+					Chartist.plugins.tooltip( {
+						appendToBody: true,
+						class: 'statify-chartist-tooltip',
+					} ),
+				],
+			} );
+
+			// Replace default points with hollow circles, add "pageview(s) to value and append date (label) as metadata.
+			chart.on( 'draw', function( d ) {
+				var circle;
+				if ( 'point' === d.type ) {
+					circle = new Chartist.Svg( 'circle', {
+						cx: [ d.x ],
+						cy: [ d.y ],
+						r: [ pointRadius ],
+						'ct:value': d.value.y + ' ' + ( d.value.y > 1 ? statifyDashboard.i18n.pageviews : statifyDashboard.i18n.pageview ),
+						'ct:meta': labels[d.index],
+					}, 'ct-point' );
+					d.element.replace( circle );
+				}
+			} );
+
+			// Render top lists.
+			if ( referrerTable ) {
+				// Get pre-existing rows.
+				rows = referrerTable.querySelectorAll( 'tr' );
+
+				// Update or append rows.
+				data.referrer.forEach( function( r, idx ) {
+					row = document.createElement( 'TR' );
+					row.innerHTML = '<td class="b">' + r.count + '</td>' +
+						'<td class="t"><a href="' + r.url + '" target="_blank"  rel="noopener noreferrer">' + r.host + '</td>';
+					if ( rows.length > idx ) {
+						referrerTable.replaceChild( row, rows[idx] );
+					} else {
+						referrerTable.appendChild( row );
+					}
+				} );
+
+				// Remove excess rows.
+				for ( i = data.referrer.length; i < rows.length; i++ ) {
+					referrerTable.removeChild( rows[i] );
+				}
+			}
+
+			if ( targetTable ) {
+				rows = targetTable.querySelectorAll( 'tr' );
+
+				data.target.forEach( function( r, idx ) {
+					row = document.createElement( 'TR' );
+					row.innerHTML = '<td class="b">' + r.count + '</td>' +
+						'<td class="t"><a href="' + r.url + '" target="_blank"  rel="noopener noreferrer">' + r.url + '</td>';
+					if ( rows.length > idx ) {
+						targetTable.replaceChild( row, rows[idx] );
+					} else {
+						targetTable.appendChild( row );
+					}
+				} );
+				for ( i = data.target.length; i < rows.length; i++ ) {
+					targetTable.removeChild( rows[i] );
+				}
+			}
+
+			if ( totalsTable ) {
+				rows = totalsTable.querySelectorAll( 'tr' );
+				row = document.createElement( 'TR' );
+				row.innerHTML = '<td class="b">' + data.totals.today + '</td>' +
+					'<td class="t">' + statifyDashboard.i18n.today + '</td>';
+				if ( rows.length > 0 ) {
+					totalsTable.replaceChild( row, rows[0] );
+				} else {
+					totalsTable.appendChild( row );
+				}
+				row = document.createElement( 'TR' );
+				row.innerHTML = '<td class="b">' + data.totals.alltime + '</td>' +
+					'<td class="t">' + statifyDashboard.i18n.since + ' ' + data.totals.since + '</td>';
+				if ( rows.length > 1 ) {
+					totalsTable.replaceChild( row, rows[1] );
+				} else {
+					totalsTable.appendChild( row );
+				}
+				for ( i = 2; i < rows.length; i++ ) {
+					totalsTable.removeChild( rows[i] );
+				}
+			}
+
+			// Re-enable refresh button.
+			if ( refreshBtn ) {
+				refreshBtn.disabled = false;
+			}
+		} ).catch( function() {
+			// Failed to load.
+			chartElem.innerHTML = '<p>' + statifyDashboard.i18n.error + '</p>';
+		} );
 	}
 
-	// Draw chart.
-	chart = new Chartist.LineChart( '#statify_chart', {
-		labels: labels,
-		series: [
-			data,
-		],
-	}, {
-		low: 0,
-		showArea: true,
-		fullWidth: fullWidth,
-		width: ( fullWidth ? undefined : 5 * data.length ),
-		axisX: {
-			showGrid: false,
-			showLabel: false,
-			offset: 0,
-		},
-		axisY: {
-			showGrid: true,
-			showLabel: true,
-			type: Chartist.FixedScaleAxis,
-			low: 0,
-			high: maxValue + 1,
-			ticks: [
-				0,
-				Math.round( maxValue * 1 / 4 ),
-				Math.round( maxValue * 2 / 4 ),
-				Math.round( maxValue * 3 / 4 ),
-				maxValue,
-			],
-			offset: 30,
-		},
-		plugins: [
-			Chartist.plugins.tooltip( {
-				appendToBody: true,
-				class: 'statify-chartist-tooltip',
-			} ),
-		],
-	} );
+	// Bind update function to "refresh" button.
+	if ( refreshBtn ) {
+		refreshBtn.addEventListener( 'click', function( evt ) {
+			evt.preventDefault();
+			updateDashboard( true );
 
-	// Replace default points with hollow circles, add "pageview(s) to value and append date (label) as meta data.
-	chart.on( 'draw', function( d ) {
-		var circle;
-		if ( 'point' === d.type ) {
-			circle = new Chartist.Svg( 'circle', {
-				cx: [ d.x ],
-				cy: [ d.y ],
-				r: [ pointRadius ],
-				'ct:value': d.value.y + ' ' + ( d.value.y > 1 ? statify_translations.pageviews : statify_translations.pageview ),
-				'ct:meta': labels[d.index],
-			}, 'ct-point' );
-			d.element.replace( circle );
-		}
-	} );
+			return false;
+		} );
+	}
+
+	// Initial update.
+	updateDashboard( false );
 }() );

views/widget-front.php
@@ -10,108 +10,49 @@
 // Quit if accessed outside WP context.
 class_exists( 'Statify' ) || exit;
 
-// Get stats.
-$refresh = isset( $_POST['statify-fresh'] ) && check_admin_referer( 'statify-dashboard-refresh' );
-$stats = Statify_Dashboard::get_stats( $refresh ); ?>
+$limit       = (int) Statify::$_options['limit'];
+$show_totals = (int) Statify::$_options['show_totals'];
+?>
 
 	<div id="statify_chart">
-		<?php if ( empty( $stats['visits'] ) ) { ?>
-			<p>
-				<?php esc_html_e( 'No data available.', 'statify' ); ?>
-			</p>
-		<?php } else { ?>
-			<table id="statify_chart_data">
-				<?php foreach ( (array) $stats['visits'] as $visit ) { ?>
-					<tr>
-						<th><?php echo esc_html( Statify::parse_date( $visit['date'] ) ); ?></th>
-						<td><?php echo (int) $visit['count']; ?></td>
-					</tr>
-				<?php } ?>
-			</table>
-		<?php } ?>
+		<span class="spinner is-active" title="<?php esc_html_e( 'loading', 'statify' ); ?>"></span>
 	</div>
 
+<?php if ( $limit > 0 ) : ?>
 
-<?php if ( ! empty( $stats['referrer'] ) ) { ?>
 	<div class="table referrer">
-		<p class="sub">
-			<?php esc_html_e( 'Top referers', 'statify' ); ?>
-		</p>
-
-		<div>
-			<table>
-				<?php foreach ( (array) $stats['referrer'] as $referrer ) { ?>
-					<tr>
-						<td class="b">
-							<?php echo (int) $referrer['count']; ?>
-						</td>
-						<td class="t">
-							<a href="<?php echo esc_url( $referrer['url'] ); ?>" target="_blank"  rel="noopener noreferrer">
-								<?php echo esc_html( $referrer['host'] ); ?>
-							</a>
-						</td>
+		<p class="sub"><?php esc_html_e( 'Top referers', 'statify' ); ?></p>
+		<table>
+			<tbody>
+				<?php for ( $i = 0; $i < $limit; $i++ ) { ?>
+					<tr class="placeholder">
+						<td colspan="2">&nbsp;</td>
 					</tr>
 				<?php } ?>
-			</table>
-		</div>
+			</tbody>
+		</table>
 	</div>
-<?php } ?>
 
-<?php if ( ! empty( $stats['target'] ) ) { ?>
 	<div class="table target">
-		<p class="sub">
-			<?php esc_html_e( 'Top targets', 'statify' ); ?>
-		</p>
-
-		<div>
-			<table>
-				<?php foreach ( (array) $stats['target'] as $target ) { ?>
-					<tr>
-						<td class="b">
-							<?php echo (int) $target['count']; ?>
-						</td>
-						<td class="t">
-							<a href="<?php echo esc_url( home_url( $target['url'] ) ); ?>" target="_blank" rel="noopener noreferrer">
-								<?php echo esc_html( $target['url'] ); ?>
-							</a>
-						</td>
-					</tr>
+		<p class="sub"><?php esc_html_e( 'Top targets', 'statify' ); ?></p>
+		<table>
+			<tbody>
+				<?php for ( $i = 0; $i < $limit; $i++ ) { ?>
+					<tr class="placeholder"><td colspan="2">&nbsp;</td></tr>
 				<?php } ?>
-			</table>
-		</div>
+			</tbody>
+		</table>
 	</div>
-<?php } ?>
 
-<?php if ( ! empty( $stats['visit_totals'] ) ) { ?>
+<?php endif; if ( $show_totals ) : ?>
 	<div class="table total">
-		<p class="sub">
-			<?php esc_html_e( 'Totals', 'statify' ); ?>
-		</p>
-		<div>
-			<table>
-				<tr>
-					<td class="b">
-						<?php echo (int) $stats['visit_totals']['today']; ?>
-					</td>
-					<td class="t">
-						<?php esc_html_e( 'today', 'statify' ); ?>
-					</td>
-				</tr>
-				<tr>
-					<td class="b">
-						<?php echo (int) $stats['visit_totals']['since_beginning']['count']; ?>
-					</td>
-					<td class="t">
-						<?php esc_html_e( 'since', 'statify' ); ?>
-						<?php echo esc_html( Statify::parse_date( $stats['visit_totals']['since_beginning']['date'] ) ); ?>
-					</td>
-				</tr>
-			</table>
-		</div>
+		<p class="sub"><?php esc_html_e( 'Totals', 'statify' ); ?></p>
+		<table>
+			<tbody>
+				<tr class="placeholder"><td colspan="2">&nbsp;</td></tr>
+				<tr class="placeholder"><td colspan="2">&nbsp;</td></tr>
+		</table>
 	</div>
-<?php } ?>
+<?php endif; ?>
 
-<form method="post">
-	<?php wp_nonce_field( 'statify-dashboard-refresh' ); ?>
-	<button type="submit" class="button button-primary" name="statify-fresh"><?php esc_html_e( 'Refresh', 'statify' ); ?></button>
-</form>
+	<button type="button" class="button button-primary" id="statify_refresh"><?php esc_html_e( 'Refresh', 'statify' ); ?></button>

commit 42ca6c3f9e2466ddf5c7249505f123fa2554d075

fix: also create the `stats` API endpoint for no-JS tracking (#270) (#271)
The `stats` endpoint was added to the API initialization hook which is
only registered, if JS-tracking is enabled. This is independent of the
tracking method actually as it's used for the dashboard widget.
Move the condition into the init hook and register it unconditionally.

Fixes: 92a229425cadb6e0be5ec513f455abdb570d6c9b

inc/class-statify-api.php
@@ -32,16 +32,18 @@ class Statify_Api extends Statify {
 	 * @return void
 	 */
 	public static function init() {
-		register_rest_route(
-			self::REST_NAMESPACE,
-			self::REST_ROUTE_TRACK,
-			array(
-				'methods'             => WP_REST_Server::CREATABLE,
-				'accept_json'         => true,
-				'callback'            => array( __CLASS__, 'track_visit' ),
-				'permission_callback' => '__return_true',
-			)
-		);
+		if ( Statify::is_javascript_tracking_enabled() ) {
+			register_rest_route(
+				self::REST_NAMESPACE,
+				self::REST_ROUTE_TRACK,
+				array(
+					'methods'             => WP_REST_Server::CREATABLE,
+					'accept_json'         => true,
+					'callback'            => array( __CLASS__, 'track_visit' ),
+					'permission_callback' => '__return_true',
+				)
+			);
+		}
 
 		register_rest_route(
 			self::REST_NAMESPACE,

inc/class-statify.php
@@ -89,9 +89,7 @@ class Statify {
 				add_filter( 'amp_post_template_analytics', array( 'Statify_Frontend', 'amp_post_template_analytics' ) );
 			}
 			// Initialize REST API.
-			if ( self::is_javascript_tracking_enabled() ) {
-				add_filter( 'rest_api_init', array( 'Statify_Api', 'init' ) );
-			}
+			add_filter( 'rest_api_init', array( 'Statify_Api', 'init' ) );
 		}
 	}
 