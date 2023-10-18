1535ea5f15c1df840395dc2d4a0e73002de06ab7
Justin Kostka <128225253+jujoko7CF@users.noreply.github.com>
2023-10-18 12:18:07 +0200

    Add Statify:get_meta() functions

--- inc/class-statify.php
+++ inc/class-statify.php
@@ -396,4 +396,23 @@ class Statify {
 
 		return false;
 	}
+
+    /**
+     * Retrieves statify metadata for the given statify ID.
+     *
+     * @param int    $statify_id Statify ID.
+     * @param string $meta_key   Optional. The meta key to retrieve. By default,
+     *                              returns data for all keys. Default empty.
+     * @param bool   $single     Optional. Whether to return a single value.
+     *                              This parameter has no effect if `$key` is not specified.
+     *                              Default false.
+     *
+     * @return mixed An array of values if `$single` is false.
+     *               The value of the meta field if `$single` is true.
+     *               False for an invalid `$statify_id` (non-numeric, zero, or negative value).
+     *               An empty string if a valid but non-existing statify ID is passed.
+     */
+    public static function get_meta( $statify_id, $meta_key = '', $single = false ) {
+        return get_metadata( 'statify', $statify_id, $meta_key, $single );
+    }
 }
