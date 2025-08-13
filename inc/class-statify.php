pluginkollektiv/statify

commit 74b63c381f7f216a9956a3e299264e5c04a0191e

migrate JS tracking from WP AJAX to WP REST API (#219) (#226)
Use a custom WP REST route for tracking requests. The tracking
logic is moved up into Statify class. The API logic is separated
into a new class, so additional endpoints can be added there.

Override the default nonce behavior for the tracking endpoint to
preserve the logged-in flag with nonce verification disabled.

inc/class-statify.php
@@ -63,10 +63,7 @@ class Statify {
 		// Cron.
 		add_action( 'statify_cleanup', array( 'Statify_Cron', 'cleanup_data' ) );
 
-		if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
-			add_action( 'wp_ajax_nopriv_statify_track', array( 'Statify_Frontend', 'track_visit_ajax' ) );
-			add_action( 'wp_ajax_statify_track', array( 'Statify_Frontend', 'track_visit_ajax' ) );
-		} elseif ( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST ) {  // XMLRPC.
+		if ( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST ) {  // XMLRPC.
 			add_filter( 'xmlrpc_methods', array( 'Statify_XMLRPC', 'xmlrpc_methods' ) );
 		} elseif ( is_admin() ) {   // Backend.
 			add_action( 'wp_initialize_site', array( 'Statify_Install', 'init_site' ) );
@@ -86,7 +83,82 @@ class Statify {
 				add_filter( 'amp_analytics_entries', array( 'Statify_Frontend', 'amp_analytics_entries' ) );
 				add_filter( 'amp_post_template_analytics', array( 'Statify_Frontend', 'amp_post_template_analytics' ) );
 			}
+			// Initialize REST API.
+			if ( self::is_javascript_tracking_enabled() ) {
+				add_filter( 'rest_api_init', array( 'Statify_Api', 'init' ) );
+			}
 		}
+	}
+
+	/**
+	 * Track the page view.
+	 *
+	 * @param string|null $referrer Referrer URL.
+	 * @param string|null $target   Target URL.
+	 *
+	 * @return void
+	 *
+	 * @since  0.1.0
+	 * @since  1.7.0 $is_snippet parameter added.
+	 * @since  2.0.0 Migration from Statify_Frontend::track_visit to Statify::track with multiple parameters.
+	 */
+	protected static function track( $referrer, $target ) {
+		// Fallbacks for uninitialized or omitted target and referrer values.
+		if ( is_null( $target ) ) {
+			$target = '/';
+		}
+		if ( is_null( $referrer ) ) {
+			$referrer = '';
+		}
+
+		// Invalid target?
+		if ( empty( $target ) || ! wp_validate_redirect( $target, false ) ) {
+			return;
+		}
+
+		// Check whether tracking should be skipped for this view.
+		if ( self::skip_tracking() ) {
+			return;
+		}
+
+		// Sanitize referrer url.
+		if ( ! empty( $referrer ) && false === self::strposa( $referrer, array( home_url(), network_admin_url() ) ) ) {
+			$referrer = esc_url_raw( $referrer, array( 'http', 'https' ) );
+		} else {
+			$referrer = '';
+		}
+
+		// Relative target URL.
+		$target = user_trailingslashit( str_replace( home_url( '/', 'relative' ), '/', $target ) );
+
+		/* Global vars */
+		global $wp_rewrite;
+
+		// Trim target URL.
+		if ( $wp_rewrite->permalink_structure ) {
+			$target = wp_parse_url( $target, PHP_URL_PATH );
+		}
+
+		// Init rows.
+		$data = array(
+			'created'  => strftime( '%Y-%m-%d', current_time( 'timestamp' ) ),
+			'referrer' => $referrer,
+			'target'   => $target,
+		);
+
+		// Insert.
+		global $wpdb;
+		$wpdb->insert( $wpdb->statify, $data );
+
+		/**
+		 * Fires after a visit was stored in the database
+		 *
+		 * @param array $data
+		 * @param int   $wpdb- >insert_id
+		 *
+		 * @since 1.5.5
+		 */
+		do_action( 'statify__visit_saved', $data, $wpdb->insert_id );
 	}
 
 	/**
@@ -122,5 +194,175 @@ class Statify {
 			),
 			true
 		);
+	}
+
+	/**
+	 * Rules to skip the tracking.
+	 *
+	 * @hook   boolean  statify__skip_tracking
+	 * @see    https://wordpress.org/plugins/statify/
+	 *
+	 * @return boolean $skip_hook TRUE if NO tracking is desired.
+	 *
+	 * @since  1.2.6
+	 * @since  2.0.0 Migration from Statify_Frontend to Statify class.
+	 */
+	private static function skip_tracking() {
+		if ( function_exists( 'apply_filters_deprecated' ) ) {
+			apply_filters_deprecated( 'statify_skip_tracking', array( '' ), '1.5.0', 'statify__skip_tracking' );
+		}
+		// Skip tracking via Hook.
+		$skip_hook = apply_filters( 'statify__skip_tracking', null );
+		if ( null !== $skip_hook ) {
+			return $skip_hook;
+		}
+
+		// Skip tracking via User Agent.
+		$user_agent = filter_var(
+			( isset( $_SERVER['HTTP_USER_AGENT'] ) ? wp_unslash( $_SERVER['HTTP_USER_AGENT'] ) : '' ),
+			FILTER_SANITIZE_STRING
+		);
+		if ( is_null( $user_agent )
+			|| false === $user_agent
+			|| self::is_bot() ) {
+			return true;
+		}
+
+		// Skip tracking via Referrer check.
+		if ( self::check_referrer() ) {
+			return true;
+		}
+
+		// Skip for trackbacks and robots.
+		if ( is_trackback() || is_robots() ) {
+			return true;
+		}
+
+		// Skip logged in users, if enabled.
+		if ( self::$_options['skip']['logged_in'] && is_user_logged_in() ) {
+			return true;
+		}
+
+		// Skip for "internal" requests.
+		return self::is_internal();
+	}
+
+	/**
+	 * Checks if user agent is a bot.
+	 *
+	 * @return boolean $is_bot TRUE if user agent is a bot, FALSE if not.
+	 *
+	 * @since 1.7.0
+	 * @since 2.0.0 Migration from Statify_Frontend to Statify class, removed $user_agent parameter.
+	 */
+	private static function is_bot() {
+		$crawler_detect = new \Jaybizzle\CrawlerDetect\CrawlerDetect();
+
+		// Check the user agent of the current 'visitor' via the user agent and http_from header.
+		return $crawler_detect->isCrawler();
+	}
+
+	/**
+	 * Rules to detect internal calls to skip tracking and not print code snippet.
+	 *
+	 * @return boolean  $skip_hook TRUE if NO tracking is desired
+	 *
+	 * @since 1.6.1
+	 * @since 1.9.0 Migration from Statify_Frontend to Statify class.
+	 */
+	protected static function is_internal() {
+		// Skip for preview, 404 calls, feed, search, favicon and sitemap access.
+		return is_preview() || is_404() || is_feed() || is_search()
+			|| ( function_exists( 'is_favicon' ) && is_favicon() )
+			|| '' !== get_query_var( 'sitemap' ) || '' !== get_query_var( 'sitemap-stylesheet' );
+	}
+
+	/**
+	 * Compare the referrer URL to the disallowed keys list.
+	 * De/activate this feature via settings in the Dashboard widget.
+	 *
+	 * @return  boolean TRUE of referrer matches disallowed keys entry and should thus be excluded.
+	 *
+	 * @since 1.5.0
+	 * @since 1.9.0 Migration from Statify_Frontend to Statify class.
+	 */
+	private static function check_referrer() {
+		// Return false if the disallowed-keys filter (formerly blacklist) is inactive.
+		if ( ! self::$_options['blacklist'] ) {
+			return false;
+		}
+
+		$referrer = filter_var(
+			( isset( $_SERVER['HTTP_REFERER'] ) ? wp_unslash( $_SERVER['HTTP_REFERER'] ) : '' ),
+			FILTER_SANITIZE_URL
+		);
+		if ( ! is_null( $referrer ) && false !== $referrer ) {
+			$referrer = wp_parse_url( $referrer, PHP_URL_HOST );
+		}
+
+		// Fallback for wp_parse_url() returning array instead of host only.
+		if ( is_array( $referrer ) && isset( $referrer['host'] ) ) {
+			$referrer = $referrer['host'];
+		}
+
+		// Return false if there still is no referrer to checj.
+		if ( ! is_string( $referrer ) ) {
+			return false;
+		}
+
+		// Finally compare referrer against the disallowed keys.
+		$disallowed_keys = self::get_disallowed_keys();
+		foreach ( $disallowed_keys as $item ) {
+			if ( strpos( $referrer, $item ) !== false ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get a array from the disallowed_keys option of 'Settings' - 'Discussion' - 'Disallowed Comment Keys'.
+	 *
+	 * @return array
+	 *
+	 * @since 1.7.3 Renamed to "get_disallowed_keys" to match WP 5.5. wording.
+	 * @since 1.9.0 Migration from Statify_Frontend to Statify class.
+	 */
+	private static function get_disallowed_keys() {
+		$disallowed_keys = get_option( 'disallowed_keys' );
+
+		if ( false === $disallowed_keys ) {
+			// WordPress < 5.5 uses the old key.
+			$disallowed_keys = get_option( 'blacklist_keys' );
+		}
+
+		$disallowed_keys = trim( $disallowed_keys );
+
+		if ( empty( $disallowed_keys ) ) {
+			return array();
+		}
+
+		return (array) explode( "\n", $disallowed_keys );
+	}
+
+	/**
+	 * Find the position of the first occurrence of a substring in a string about a array.
+	 *
+	 * @param string  $haystack The string to search in.
+	 * @param array   $needle   The string to search for.
+	 * @param integer $offset   Search will start this number of characters counted from the beginning of the string.
+	 *
+	 * @return boolean
+	 */
+	private static function strposa( $haystack, array $needle, $offset = 0 ) {
+
+		foreach ( $needle as $query ) {
+			if ( strpos( $haystack, $query, $offset ) !== false ) {
+				return true;
+			} // Stop on first true result.
+		}
+
+		return false;
 	}
 }

inc/class-statify-api.php
@@ -1,0 +1,103 @@
+<?php
+/**
+ * Statify: Statify_API class
+ *
+ * This file contains methods for REST API integration.
+ *
+ * @package Statify
+ * @since   1.9
+ */
+
+// Quit if accessed outside WP context.
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Statify REST API integration.
+ *
+ * @since 1.9
+ */
+class Statify_Api extends Statify {
+	/**
+	 * REST endpoints
+	 *
+	 * @var    string
+	 */
+	const REST_NAMESPACE   = 'statify/v1';
+	const REST_ROUTE_TRACK = 'track';
+
+	/**
+	 * Initialize REST API routes.
+	 *
+	 * @return void
+	 */
+	public static function init() {
+		register_rest_route(
+			self::REST_NAMESPACE,
+			self::REST_ROUTE_TRACK,
+			array(
+				'methods'             => WP_REST_Server::CREATABLE,
+				'accept_json'         => true,
+				'callback'            => array( __CLASS__, 'track_visit' ),
+				'permission_callback' => '__return_true',
+			)
+		);
+
+		add_filter( 'rest_authentication_errors', array( __CLASS__, 'check_authentication' ), 5 );
+	}
+
+	/**
+	 * Filters REST API authentication errors.
+	 *
+	 * Override the default authentication check for the tracking endpoint.
+	 * Requests are unauthenticated by default, if no nonce is provided. Verification can be disabled in the plugin
+	 * configuration, and we still need the information, if tue current user is logged in.
+	 * We don't make any decision for other routes.
+	 *
+	 * @param WP_Error|null|true $errors WP_Error if authentication error, null if authentication
+	 *                                   method wasn't used, true if authentication succeeded.
+	 */
+	public static function check_authentication( $errors ) {
+		$route = untrailingslashit( $GLOBALS['wp']->query_vars['rest_route'] );
+
+		if ( '/' . self::REST_NAMESPACE . '/' . self::REST_ROUTE_TRACK === $route ) {
+			// We disable verification for the tracking route.
+			return true;
+		}
+
+		// Don't change behavior for other routes.
+		return $errors;
+	}
+
+	/**
+	 * Track the page view via API.
+	 *
+	 * @param WP_REST_Request $request The request.
+	 *
+	 * @return WP_REST_Response The response.
+	 */
+	public static function track_visit( $request ) {
+		// Only do something if snippet use is actually configured.
+		if ( Statify::is_javascript_tracking_enabled() ) {
+			// Nonce verification, if necessary. We do not rely on the WP REST default mechanisms.
+			if ( Statify::TRACKING_METHOD_JAVASCRIPT_WITH_NONCE_CHECK === self::$_options['snippet'] ) {
+				$nonce = $request->get_param( 'nonce' );
+				if ( empty( $nonce ) || false === wp_verify_nonce( $nonce, 'statify_track' ) ) {
+					return new WP_REST_Response( null, 403 );
+				}
+			}
+
+			$referrer = $request->get_param( 'referrer' );
+			if ( null !== $referrer ) {
+				$referrer = filter_var( $referrer, FILTER_SANITIZE_URL );
+			}
+			$target   = $request->get_param( 'target' );
+			if ( null !== $target ) {
+				$target = filter_var( $target, FILTER_SANITIZE_URL );
+			}
+
+			Statify::track( $referrer, $target );
+		}
+
+		return new WP_REST_Response( null, 204 );
+	}
+}

inc/class-statify-frontend.php
@@ -25,306 +25,27 @@ class Statify_Frontend extends Statify {
 	 * @since    1.7.0 $is_snippet parameter added.
 	 * @version  1.7.0
 	 *
-	 * @param boolean $is_snippet Is tracking triggered via JS (default: false).
+	 * @param boolean $is_snippet [deprecated] Is tracking triggered via JS (default: false).
 	 *
-	 * @return   boolean
+	 * @return boolean
 	 */
 	public static function track_visit( $is_snippet = false ) {
+		if ( self::is_javascript_tracking_enabled() ) {
+			return false;
+		}
+
 		// Set target & referrer.
 		$target   = null;
 		$referrer = null;
-		if ( self::is_javascript_tracking_enabled() ) {
-			if ( ! $is_snippet ) {
-				return false;
-			}
-
-			if ( isset( $_REQUEST['statify_target'] ) ) {
-				$target = filter_var( wp_unslash( $_REQUEST['statify_target'] ), FILTER_SANITIZE_URL );
-			}
-			if ( isset( $_REQUEST['statify_referrer'] ) ) {
-				$referrer = filter_var( wp_unslash( $_REQUEST['statify_referrer'] ), FILTER_SANITIZE_URL );
-			}
-		} else {
-			if ( isset( $_SERVER['REQUEST_URI'] ) ) {
-				$target = filter_var( wp_unslash( $_SERVER['REQUEST_URI'] ), FILTER_SANITIZE_URL );
-			}
-			if ( isset( $_SERVER['HTTP_REFERER'] ) ) {
-				$referrer = filter_var( wp_unslash( $_SERVER['HTTP_REFERER'] ), FILTER_SANITIZE_URL );
-			}
+		if ( isset( $_SERVER['REQUEST_URI'] ) ) {
+			$target = filter_var( wp_unslash( $_SERVER['REQUEST_URI'] ), FILTER_SANITIZE_URL );
+		}
+		if ( isset( $_SERVER['HTTP_REFERER'] ) ) {
+			$referrer = filter_var( wp_unslash( $_SERVER['HTTP_REFERER'] ), FILTER_SANITIZE_URL );
 		}
 
-		// Fallbacks for uninitialized or omitted target and referrer values.
-		if ( is_null( $target ) || false === $target ) {
-			$target = '/';
-		}
-		if ( is_null( $referrer ) || false === $referrer ) {
-			$referrer = '';
-		}
-
-		/* Invalid target? */
-		if ( empty( $target ) || ! wp_validate_redirect( $target, false ) ) {
-			return self::_jump_out( $is_snippet );
-		}
-
-		/* Check whether tracking should be skipped for this view. */
-		if ( self::_skip_tracking() ) {
-			return self::_jump_out( $is_snippet );
-		}
-
-		/* Global vars */
-		global $wpdb, $wp_rewrite;
-
-		/* Init rows */
-		$data = array(
-			'created'  => '',
-			'referrer' => '',
-			'target'   => '',
-		);
-
-		// Set request timestamp.
-		$data['created'] = current_time( 'Y-m-d' );
-
-		$needles = array( home_url(), network_admin_url() );
-
-		// Sanitize referrer url.
-		if ( ! empty( $referrer ) && self::strposa( $referrer, $needles ) === false ) {
-			$data['referrer'] = esc_url_raw( $referrer, array( 'http', 'https' ) );
-		}
-
-		/* Relative target url */
-		$data['target'] = user_trailingslashit( str_replace( home_url( '/', 'relative' ), '/', $target ) );
-
-		// Trim target url.
-		if ( $wp_rewrite->permalink_structure ) {
-			$data['target'] = wp_parse_url( $data['target'], PHP_URL_PATH );
-		}
-
-		// Sanitize target url.
-		$data['target'] = esc_url_raw( $data['target'] );
-
-		// Insert.
-		$wpdb->insert( $wpdb->statify, $data );
-
-		/**
-		 * Fires after a visit was stored in the database
-		 *
-		 * @since 1.5.5
-		 *
-		 * @param array $data
-		 * @param int $wpdb->insert_id
-		 */
-		do_action( 'statify__visit_saved', $data, $wpdb->insert_id );
-
-		// Jump!
-		return self::_jump_out( $is_snippet );
+		Statify::track( $referrer, $target );
 	}
-
-	/**
-	 * Track the page view via AJAX.
-	 *
-	 * @return void
-	 */
-	public static function track_visit_ajax() {
-		// Only do something if snippet use is actually configured.
-		if ( ! self::is_javascript_tracking_enabled() ) {
-			return;
-		}
-
-		// Check AJAX referrer.
-		if ( Statify::TRACKING_METHOD_JAVASCRIPT_WITH_NONCE_CHECK === self::$_options['snippet'] ) {
-			check_ajax_referer( 'statify_track' );
-		}
-
-		self::track_visit( true );
-	}
-
-	/**
-	 * Find the position of the first occurrence of a substring in a string about a array.
-	 *
-	 * @param string $haystack The string to search in.
-	 * @param array  $needle   The string to search for.
-	 * @param int    $offset   Search will start this number of characters counted from the beginning of the string.
-	 *
-	 * @return bool
-	 */
-	private static function strposa( $haystack, array $needle, $offset = 0 ) {
-
-		foreach ( $needle as $query ) {
-			if ( strpos( $haystack, $query, $offset ) !== false ) {
-				return true;
-			} // Stop on first true result.
-		}
-
-		return false;
-	}
-
-	/**
-	 * Rules to skip the tracking
-	 *
-	 * @since    1.2.6
-	 * @version  1.6.3
-	 *
-	 * @hook     boolean  statify__skip_tracking
-	 * @see      https://wordpress.org/plugins/statify/
-	 *
-	 * @return   boolean  $skip_hook  TRUE if NO tracking is desired
-	 */
-	private static function _skip_tracking() {
-
-		if ( function_exists( 'apply_filters_deprecated' ) ) {
-			apply_filters_deprecated( 'statify_skip_tracking', array( '' ), '1.5.0', 'statify__skip_tracking' );
-		}
-		// Skip tracking via Hook.
-		$skip_hook = apply_filters( 'statify__skip_tracking', null );
-		if ( null !== $skip_hook ) {
-			return $skip_hook;
-		}
-
-		// Skip tracking via User Agent.
-		$user_agent = sanitize_text_field( isset( $_SERVER['HTTP_USER_AGENT'] ) ? wp_unslash( $_SERVER['HTTP_USER_AGENT'] ) : '' );
-		if ( is_null( $user_agent )
-			|| false === $user_agent
-			|| self::is_bot() ) {
-			return true;
-		}
-
-		// Skip tracking via Referrer check.
-		if ( self::check_referrer() ) {
-			return true;
-		}
-
-		// Skip for trackbacks and robots.
-		if ( is_trackback() || is_robots() ) {
-			return true;
-		}
-
-		// Skip logged in users, if enabled.
-		if ( self::$_options['skip']['logged_in'] && is_user_logged_in() ) {
-			return true;
-		}
-
-		// Skip for "internal" requests.
-		return self::_is_internal();
-	}
-
-	/**
-	 * Checks if user agent is a bot.
-	 *
-	 * @since 1.7.0
-	 *
-	 * @return boolean $is_bot     true if user agent is a bot, false if not.
-	 */
-	private static function is_bot() {
-		$crawler_detect = new \Jaybizzle\CrawlerDetect\CrawlerDetect();
-
-		// Check the user agent of the current 'visitor' via the user agent and http_from header.
-		return $crawler_detect->isCrawler();
-	}
-
-	/**
-	 * Rules to detect internal calls to skip tracking and not print code snippet.
-	 *
-	 * @since    1.6.1
-	 *
-	 * @return   boolean  $skip_hook  TRUE if NO tracking is desired
-	 */
-	private static function _is_internal() {
-		// Skip for preview, 404 calls, feed, search, favicon and sitemap access.
-		return is_preview() || is_404() || is_feed() || is_search()
-			|| ( function_exists( 'is_favicon' ) && is_favicon() )
-			|| '' !== get_query_var( 'sitemap' ) || '' !== get_query_var( 'sitemap-stylesheet' );
-	}
-
-	/**
-	 * Compare the referrer URL to the disallowed keys list.
-	 * De/activate this feature via settings in the Dashboard widget.
-	 *
-	 * @since   1.5.0
-	 * @version 1.6.3
-	 *
-	 * @return  boolean TRUE of referrer matches disallowed keys entry and should thus be excluded.
-	 */
-	private static function check_referrer() {
-
-		// Return false if the disallowed-keys filter (formerly blacklist) is inactive.
-		if ( ! self::$_options['blacklist'] ) {
-			return false;
-		}
-
-		$referrer = filter_var(
-			( isset( $_SERVER['HTTP_REFERER'] ) ? wp_unslash( $_SERVER['HTTP_REFERER'] ) : '' ),
-			FILTER_SANITIZE_URL
-		);
-		if ( ! is_null( $referrer ) && false !== $referrer ) {
-			$referrer = wp_parse_url( $referrer, PHP_URL_HOST );
-		}
-
-		// Fallback for wp_parse_url() returning array instead of host only.
-		if ( is_array( $referrer ) && isset( $referrer['host'] ) ) {
-			$referrer = $referrer['host'];
-		}
-
-		// Return false if there still is no referrer to checj.
-		if ( ! is_string( $referrer ) ) {
-			return false;
-		}
-
-		// Finally compare referrer against the disallowed keys.
-		$disallowed_keys = self::get_disallowed_keys();
-		foreach ( $disallowed_keys as $item ) {
-			if ( strpos( $referrer, $item ) !== false ) {
-				return true;
-			}
-		}
-
-		return false;
-	}
-
-	/**
-	 * Get a array from the disallowed_keys option of 'Settings' - 'Discussion' - 'Disallowed Comment Keys'.
-	 *
-	 * @since  2016-12-21
-	 * @since 1.7.3 Renamed to "get_disallowed_keys" to match WP 5.5. wording.
-	 *
-	 * @return array
-	 */
-	private static function get_disallowed_keys() {
-		$disallowed_keys = get_option( 'disallowed_keys' );
-
-		if ( false === $disallowed_keys ) {
-			// WordPress < 5.5 uses the old key.
-			$disallowed_keys = get_option( 'blacklist_keys' );
-		}
-
-		$disallowed_keys = trim( $disallowed_keys );
-
-		if ( empty( $disallowed_keys ) ) {
-			return array();
-		}
-
-		return (array) explode( "\n", $disallowed_keys );
-	}
-
-	/**
-	 * Send JavaScript headers or return false
-	 *
-	 * @since    1.1.0
-	 * @version  1.3.1
-	 *
-	 * @param   boolean $is_snippet Snippet type.
-	 *
-	 * @return  mixed  Exit or return depending on snippet type.
-	 */
-	private static function _jump_out( $is_snippet ) {
-
-		if ( $is_snippet ) {
-			nocache_headers();
-			wp_die( '', '', 204 );
-		}
-
-		return false;
-	}
-
 
 	/**
 	 * Declare GET variables for further use
@@ -337,7 +58,6 @@ class Statify_Frontend extends Statify {
 	 * @return  array  $vars  Output with plugin variables
 	 */
 	public static function query_vars( $vars ) {
-
 		$vars[] = 'statify_referrer';
 		$vars[] = 'statify_target';
 
@@ -362,7 +82,7 @@ class Statify_Frontend extends Statify {
 		}
 
 		// Skip by internal rules (#84).
-		if ( self::_is_internal() ) {
+		if ( self::is_internal() ) {
 			return;
 		}
 
@@ -375,14 +95,13 @@ class Statify_Frontend extends Statify {
 		);
 
 		// Add endpoint to script.
-		wp_localize_script(
-			'statify-js',
-			'statify_ajax',
-			array(
-				'url'   => admin_url( 'admin-ajax.php' ),
-				'nonce' => wp_create_nonce( 'statify_track' ),
-			)
+		$script_data = array(
+			'url' => esc_url_raw( rest_url( Statify_Api::REST_NAMESPACE . '/' . Statify_Api::REST_ROUTE_TRACK ) ),
 		);
+		if ( Statify::TRACKING_METHOD_JAVASCRIPT_WITH_NONCE_CHECK === self::$_options['snippet'] ) {
+			$script_data['nonce'] = wp_create_nonce( 'statify_track' );
+		}
+		wp_localize_script( 'statify-js', 'statifyAjax', $script_data );
 	}
 
 	/**
@@ -438,15 +157,13 @@ class Statify_Frontend extends Statify {
 	 * @return array Configuration array.
 	 */
 	private static function make_amp_config() {
-		return array(
+		$cfg = array(
 			'requests'       => array(
-				'pageview' => admin_url( 'admin-ajax.php' ),
+				'pageview' => rest_url( Statify_Api::REST_NAMESPACE . '/' . Statify_Api::REST_ROUTE_TRACK ),
 			),
 			'extraUrlParams' => array(
-				'action'           => 'statify_track',
-				'_ajax_nonce'      => wp_create_nonce( 'statify_track' ),
-				'statify_referrer' => '${documentReferrer}',
-				'statify_target'   => '${canonicalPath}amp/',
+				'referrer' => '${documentReferrer}',
+				'target'   => '${canonicalPath}amp/',
 			),
 			'triggers'       => array(
 				'trackPageview' => array(
@@ -460,5 +177,11 @@ class Statify_Frontend extends Statify {
 				'image'   => false,
 			),
 		);
+
+		if ( Statify::TRACKING_METHOD_JAVASCRIPT_WITH_NONCE_CHECK === self::$_options['snippet'] ) {
+			$cfg['extraUrlParams']['nonce'] = wp_create_nonce( 'statify_track' );
+		}
+
+		return $cfg;
 	}
 }

js/snippet.js
@@ -1,15 +1,18 @@
 ( function() {
 	var statifyReq;
+	var data;
 	try {
 		statifyReq = new XMLHttpRequest();
-		statifyReq.open( 'POST', statify_ajax.url, true );
-		statifyReq.setRequestHeader( 'Content-Type', 'application/x-www-form-urlencoded;' );
-		statifyReq.send(
-			'_ajax_nonce=' + statify_ajax.nonce +
-			'&action=statify_track' +
-			'&statify_referrer=' + encodeURIComponent( document.referrer ) +
-			'&statify_target=' + encodeURIComponent( location.pathname + location.search )
-		);
+		statifyReq.open( 'POST', statifyAjax.url, true );
+		statifyReq.setRequestHeader( 'Content-Type', 'application/json' );
+		data = {
+			referrer: document.referrer,
+			target: location.pathname + location.search,
+		};
+		if ( 'nonce' in statifyAjax ) {
+			data.nonce = statifyAjax.nonce;
+		}
+		statifyReq.send( JSON.stringify( data ) );
 	} catch ( e ) {
 	}
 }() );

statify.php
@@ -7,7 +7,7 @@
  * Author URI:  https://pluginkollektiv.org/
  * Plugin URI:  https://statify.pluginkollektiv.org/
  * License:     GPLv3 or later
- * Version:     1.8.4
+ * Version:     1.9.0
  *
  * @package WordPress
  */
@@ -20,7 +20,7 @@ defined( 'ABSPATH' ) || exit;
 define( 'STATIFY_FILE', __FILE__ );
 define( 'STATIFY_DIR', dirname( __FILE__ ) );
 define( 'STATIFY_BASE', plugin_basename( __FILE__ ) );
-define( 'STATIFY_VERSION', '1.8.4' );
+define( 'STATIFY_VERSION', '1.9.0' );
 
 
 /* Hooks */
@@ -68,6 +68,7 @@ function statify_autoload( $class ) {
 
 	$plugin_classes = array(
 		'Statify',
+		'Statify_Api',
 		'Statify_Backend',
 		'Statify_Frontend',
 		'Statify_Dashboard',

tests/js/snippet.test.js
@@ -3,8 +3,8 @@ const expect = require( 'chai' ).expect;
 
 describe( 'Statify Snippet', function() {
 	beforeEach( () => {
-		global.statify_ajax = {
-			url: 'https://wp.example.com/admin-ajax.php',
+		global.statifyAjax = {
+			url: 'https://wp.example.com/wp-json/statify/v1/track',
 			nonce: '0123456789',
 		};
 		global.document = {
@@ -24,21 +24,23 @@ describe( 'Statify Snippet', function() {
 		global.XMLHttpRequest.restore();
 	} );
 
-	it( 'should issue a single POST request to the AJAX endpoint', () => {
+	it( 'should issue a single POST request to the REST endpoint', () => {
 		require( '../../js/snippet' );
 
 		expect( this.requests ).to.length( 1, 'Unexpected number of requests' );
 		expect( this.requests[0].method ).to.equal( 'POST', 'Unexpected method' );
 		expect( this.requests[0].url ).to.equal(
-			'https://wp.example.com/admin-ajax.php',
+			'https://wp.example.com/wp-json/statify/v1/track',
 			'Unexpected target URL'
 		);
 		expect( this.requests[0].requestHeaders ).to.deep.equal(
-			{ 'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8' },
+			{
+				'Content-Type': 'application/json;charset=utf-8',
+			},
 			'Unexpected request headers'
 		);
 		expect( this.requests[0].requestBody ).to.equal(
-			'_ajax_nonce=0123456789&action=statify_track&statify_referrer=https%3A%2F%2Freferrer.example.com%2Fsome%2Fpage%2F&statify_target=%2Fmy%2Fpage%2F%3Farg%3Dvalue',
+			'{"referrer":"https://referrer.example.com/some/page/","target":"/my/page/?arg=value","nonce":"0123456789"}',
 			'Unexpected request body'
 		);
 		expect( this.requests[0].async ).to.equal( true, 'Request should be async' );

commit 5be915f58ce5172771bd90f803161b6f590ee8ba

use add_action() instead of add_filter() for rest_api_init (#302)
The `rest_api_init` hook is an action hook and should be used as such.
Replace `add_filter()` with `add_action()` to register our init method.

Fixes: 74b63c381f7f216a9956a3e299264e5c04a0191e

inc/class-statify.php
@@ -87,7 +87,7 @@ class Statify {
 				add_filter( 'amp_post_template_analytics', array( 'Statify_Frontend', 'amp_post_template_analytics' ) );
 			}
 			// Initialize REST API.
-			add_filter( 'rest_api_init', array( 'Statify_Api', 'init' ) );
+			add_action( 'rest_api_init', array( 'Statify_Api', 'init' ) );
 		}
 	}
 