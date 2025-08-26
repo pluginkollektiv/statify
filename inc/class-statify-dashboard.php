pluginkollektiv/statify

commit a2b533009b9884d77051e53aca22d55ee3bebd22

Separate permissions to see widget, edit widget and edit settings
Not every user who can see stats should be able to configure every
 setting and probably not even edit the widget itself. Separated the
 permissions required to limit access to new options and potentially
 allow more flexible granting filters.

inc/class-statify-dashboard.php
@@ -42,6 +42,9 @@ class Statify_Dashboard extends Statify {
 			return;
 		}
 
+		// Check if user can edit the widget.
+		$can_edit = apply_filters( 'statify__user_can_see_stats', current_user_can( 'edit_dashboard' ) );
+
 		// Load textdomain.
 		load_plugin_textdomain(
 			'statify',
@@ -57,7 +60,7 @@ class Statify_Dashboard extends Statify {
 			'statify_dashboard',
 			'Statify',
 			array( __CLASS__, 'print_frontview' ),
-			array( __CLASS__, 'print_backview' )
+			$can_edit ? array( __CLASS__, 'print_backview' ) : null
 		);
 
 		// Init CSS.

inc/class-statify-backend.php
@@ -58,7 +58,7 @@ class Statify_Backend {
 	public static function add_action_link( $input ) {
 
 		// Rights?
-		if ( ! current_user_can( 'edit_dashboard' ) ) {
+		if ( ! current_user_can( 'manage_options' ) ) {
 			return $input;
 		}
 

views/widget-back.php
@@ -10,6 +10,7 @@
 // Quit if accessed outside WP context.
 class_exists( 'Statify' ) || exit; ?>
 
+<?php if ( current_user_can( 'manage_options' ) ) : ?>
 <p class="meta-links">
 	<a href="<?php echo esc_attr( add_query_arg( array( 'page' => 'statify-settings' ), admin_url( '/options-general.php' ) ) ); ?>"
 		title="<?php esc_attr_e( 'Open full settings page', 'statify' ); ?>">
@@ -18,6 +19,7 @@ class_exists( 'Statify' ) || exit; ?>
 </p>
 
 <br>
+<?php endif; ?>
 
 <h3><?php esc_html_e( 'Widget Settings', 'statify' ); ?></h3>
 <fieldset>

commit 4b8204cba44306731bbc32260579897d733a72b3

remove redundant permissions check for dashboard control (#304)
At the point where permissions are checked, user_can_see_stats() is
always true, unless some special hook is with dynamic response behavior
is configured.

WordPress takes care of control access and the callback also contains
a permissions check, so let's simply register both callbacks and omit
the redundant check.

Fixes: a2b533009b9884d77051e53aca22d55ee3bebd22

inc/class-statify-dashboard.php
@@ -38,9 +38,6 @@ class Statify_Dashboard extends Statify {
 			return;
 		}
 
-		// Check if user can edit the widget.
-		$can_edit = self::user_can_see_stats();
-
 		// Load textdomain.
 		load_plugin_textdomain(
 			'statify',
@@ -56,7 +53,7 @@ class Statify_Dashboard extends Statify {
 			'statify_dashboard',
 			'Statify',
 			array( __CLASS__, 'print_frontview' ),
-			$can_edit ? array( __CLASS__, 'print_backview' ) : null
+			array( __CLASS__, 'print_backview' )
 		);
 
 		// Init CSS.