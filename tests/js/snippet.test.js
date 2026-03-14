4445f02f622d8728584b37e8e6f037dfbe4cefdb
Stefan Kalscheuer <stefan@stklcode.de>
2026-03-14 14:37:56 +0100

    test: migrate from Mocha/Chai to node:test (#311)
    Built-in test and assertion modules are available since Node 18. We use
    Node 24 in our pipelines and Node 20 is the oldest supported LTS, so we
    can simplify our tool stack without any major interruptions.

--- tests/js/snippet.test.js
+++ tests/js/snippet.test.js
@@ -1,7 +1,11 @@
+const { describe, it, beforeEach, afterEach } = require('node:test');
+const assert = require('node:assert/strict');
 const sinon = require('sinon');
-const expect = require('chai').expect;
 
 describe('Statify Snippet', () => {
+	let requests = [];
+	let xhrMock;
+
 	beforeEach(() => {
 		global.statifyAjax = {
 			url: 'https://wp.example.com/wp-json/statify/v1/track',
@@ -15,37 +19,39 @@ describe('Statify Snippet', () => {
 			search: '?arg=value',
 		};
 
-		global.XMLHttpRequest = sinon.useFakeXMLHttpRequest();
-		this.requests = [];
-		global.XMLHttpRequest.onCreate = (xhr) => this.requests.push(xhr);
+		xhrMock = sinon.useFakeXMLHttpRequest();
+		requests = [];
+		xhrMock.onCreate = (xhr) => requests.push(xhr);
+		global.XMLHttpRequest = xhrMock;
 	});
 
 	afterEach(() => {
-		global.XMLHttpRequest.restore();
+		xhrMock.restore();
+		delete require.cache[require.resolve('../../js/snippet')];
 	});
 
 	it('should issue a single POST request to the REST endpoint', () => {
 		require('../../js/snippet');
 
-		expect(this.requests).to.length(1, 'Unexpected number of requests');
-		expect(this.requests[0].method).to.equal('POST', 'Unexpected method');
-		expect(this.requests[0].url).to.equal(
+		assert.equal(requests.length, 1, 'Unexpected number of requests');
+		assert.equal(requests[0].method, 'POST', 'Unexpected method');
+		assert.equal(
+			requests[0].url,
 			'https://wp.example.com/wp-json/statify/v1/track',
 			'Unexpected target URL'
 		);
-		expect(this.requests[0].requestHeaders).to.deep.equal(
+		assert.deepEqual(
+			requests[0].requestHeaders,
 			{
 				'Content-Type': 'application/json;charset=utf-8',
 			},
 			'Unexpected request headers'
 		);
-		expect(this.requests[0].requestBody).to.equal(
+		assert.equal(
+			requests[0].requestBody,
 			'{"referrer":"https://referrer.example.com/some/page/","target":"/my/page/?arg=value","nonce":"0123456789"}',
 			'Unexpected request body'
 		);
-		expect(this.requests[0].async).to.equal(
-			true,
-			'Request should be async'
-		);
+		assert.equal(requests[0].async, true, 'Request should be async');
 	});
 });
