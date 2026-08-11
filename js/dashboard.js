f39b9868144a4690af2d5a5606b4ae71db39bf01
Stefan Kalscheuer <stefan@stklcode.de>
2026-08-11 08:27:04 +0200

    fix: minor dashboard script corrections (#379)
    * Pass simple records instead of URLSearchParams to fetchData().
    * Use number values instead of arrays for SVG circle creation.
    * Do not assign "this" in arrow function reset button event listener.
      Correctly assign the event target instead of "this" which resolves to
      "document" in the arrow function.
    
    Fixes: 8843b702ac21d2ec71ade6fa88165f7a580d17e7

--- js/dashboard.js
+++ js/dashboard.js
@@ -193,10 +193,7 @@
 	 * @return {Promise<{[key: string]: number}>} Data promise from API.
 	 */
 	function loadDaily(year) {
-		return fetchData(
-			'stats/extended',
-			new URLSearchParams({ scope: 'day', year })
-		);
+		return fetchData('stats/extended', { scope: 'day', year });
 	}
 
 	/**
@@ -237,12 +234,12 @@
 	 * @return {Promise<Array<{count: number, host: string, url: string}>>} Data promise from API.
 	 */
 	function loadPerReferrer() {
-		const param = new URLSearchParams();
+		const param = {};
 		const search = new URLSearchParams(window.location.search);
 		['post', 'start', 'end'].forEach((p) => {
 			const v = search.get(p);
 			if (v) {
-				param.set(p, v);
+				param[p] = v;
 			}
 		});
 
@@ -443,9 +440,9 @@
 				circle = new Chartist.Svg(
 					'circle',
 					{
-						cx: [d.x],
-						cy: [d.y],
-						r: [pointRadius],
+						cx: d.x,
+						cy: d.y,
+						r: pointRadius,
 						'ct:value': wp.i18n.sprintf(
 							/* translators: %s: Number of page views. */
 							wp.i18n._n(
